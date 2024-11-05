1fa43834930a023450e6884ac5adb0d645b3b279
Rafael Callegari De Oliveira <137407464+CALLEGARii@users.noreply.github.com>
2024-11-05 20:48:29 -0300

    Update OuterJoins.sql

--- OuterJoins.sql
+++ OuterJoins.sql
@@ -11,13 +11,11 @@ from marinheiros m
 left join reservas r on m.codigo_marinheiro = r.codigo_marinheiro
 where r.codigo_marinheiro is null;
 
-
 -- EX3 --
 select b.nome_barco
 from barcos b
 right join reservas r on b.codigo_barco = r.codigo_barco
 where r.codigo_barco is null;
-
 
 -- EX4 --	
 create table departamento (
@@ -34,7 +32,6 @@ create table empregado (
     constraint fk_emp_dep foreign key (id_departamento) references departamento
 );
 
-
 -- EX5 --
 insert into departamento (id_departamento, nome_departamento) values (1, 'rh');
 insert into departamento (id_departamento, nome_departamento) values (2, 'vendas');
@@ -44,20 +41,17 @@ insert into empregado (id_empregado, nome_empregado, id_departamento) values (1,
 insert into empregado (id_empregado, nome_empregado, id_departamento) values (2, 'maria', 2);
 insert into empregado (id_empregado, nome_empregado, id_departamento) values (3, 'carlos', null);
 
-
 -- EX6 --
 select d.nome_departamento
 from departamento d
 left join empregado e on d.id_departamento = e.id_departamento
 where e.id_departamento is null;
 
-
 -- EX7 --
 select d.nome_departamento, e.nome_empregado
 from departamento d
 right join empregado e on d.id_departamento = e.id_departamento
 where e.id_departamento is null;
-
 
 -- EX8 --
 select d.nome_departamento, e.nome_empregado
